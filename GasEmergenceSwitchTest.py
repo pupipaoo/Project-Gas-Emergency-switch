1b16ddc74f52c0d984776e425580cd70f7cb4b29
pupipaoo <114714786+pupipaoo@users.noreply.github.com>
2022-11-24 16:43:46 +0800

    111.11.24 Project-GES

--- GasEmergenceSwitchTest.py
+++ GasEmergenceSwitchTest.py
@@ -10,16 +10,17 @@ echo = Pin(16, Pin.IN,Pin.PULL_DOWN)
 servo_1 = PWM(Pin(23))
 servo_1.freq(50)
 servoSwitch=Pin(22,Pin.OUT)
+flame_sensor = Pin(21, Pin.IN)
 
 def ping():
     trig.value(1)   #發超聲波
     time.sleep_us(10)   #延遲ms
     trig.value(0)    #超聲波
     count=0
-    timeout=False   #延遲
+    timeout=False   #
     start=time.ticks_us()
-    while echo.value() == 0: #wait for HIGH
-        start=time.ticks_us()
+    while echo.value() == 0: #用來wait for HIGH
+        start=time.ticks_us()    #tick代表計時，紀錄發出後的時間
     while echo.value() : #wait for HIGH
         stop=time.ticks_us()
     duration = stop - start
@@ -35,15 +36,18 @@ def servo(degrees):
     minDuty=1800
     newDuty=minDuty+(maxDuty-minDuty)*(degrees/180)
     servo_1.duty_u16(int(newDuty))
-    
+
+servoSwitch.value(0)
 while True:
     distance = round(ping())
     print("%s cm" %distance)
     if distance>100:
-        servoSwitch.value(1)  #電流流通
-        pin_led.value(1)
-        servo(180)  #順時針到12點
-        time.sleep(0.1)    #過5秒再從while迴圈第一航開始
+        if flame_sensor.value() == 0:   #此時有火源
+            time.sleep(1200)
+            servoSwitch.value(1)  #電流流通
+            pin_led.value(1)
+            servo(180)  #順時針到高點
+            time.sleep(0.1)    #過 秒再從while迴圈第一航開始
     else:
         servoSwitch.value(0)
         pin_led.value(0)
