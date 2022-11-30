df9ae05c7089b25d134f879686110023536280e3
pupipaoo <114714786+pupipaoo@users.noreply.github.com>
2022-11-30 15:47:45 +0800

    111.11.25 Project-GES

--- pub_sub_MQTTtest.py
+++ pub_sub_MQTTtest.py
@@ -1,10 +1,8 @@
 import network
 import time
-from machine import Pin
+from machine import Pin,Timer
 from mqttlib import MQTTClient
 
-
-#Run過一次後，須至少隔60秒才能再跑程式，這是板子內部的Wifi Internal設定
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect("My ASUS","jade1234")
@@ -12,7 +10,7 @@ time.sleep(10)
 print('wifi OK:',wlan.isconnected())
 print(wlan.ifconfig())
 
-mqtt_server = 'hq.ittraining.com.tw'
+mqtt_server = 'test.mosquitto.org'
 #mqtt_server = 'hq.ittraining.com.tw'
 client_id = 'pico_mqtt_iot' #裝置上(pico)的ID，自己隨便取，假設有多個裝置下，可以用來做區別
 topic_pub = b'sensor/fire'   #b是指binary 
@@ -27,38 +25,35 @@ def mqtt_connect():
 
 def reconnect():
     print('Failed to connect to the MQTT Broker. Reconnecting...')
-    time.sleep(5)
+    time.sleep(10)
     machine.reset()
 
-def on_message(topic, msg):
+def check_socket(x):
+    client.check_msg()
+
+def on_message(topic, msg): #MQTT發布功能不能選擇保留訊息
     print(topic,msg)
     if topic==topic_sub:
         on=msg.decode('utf-8')
         print(on)
+        if on=='0':
+            led.value(0)
         if on=='1':
             led.value(1)
-        elif on=='0':
-            led.value(0)       
-
 
 try:
     client = mqtt_connect()
     client.set_callback(on_message)
     client.subscribe(topic_sub)
     #print(client.subscribe(topic_sub))
+    
     led.value(1)
 except OSError as e:
     reconnect()
 
+tim2=Timer(1)
+tim2.init(period=500,mode=Timer.PERIODIC,callback=check_socket) #設置有Timer的thread去跑確認socket函示
 led.value(1)
 while True:
     client.publish(topic_pub,"on1")
     time.sleep(0.5)
-    #led.value(1)
-    #time.sleep_ms(1000)
- 
-    #time.sleep_ms(1000)
-    #led.value(0)
-    client.check_msg()#透過語法讓程式主動檢查socket，也就是檢查底層有沒有訊息過來 ，因為在mqtt架構下，有訊息從broker丟來時，mqtt不會自主通知
-
-    
